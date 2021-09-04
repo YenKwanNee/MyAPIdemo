3ee96357211dbc3b3d6f098a95f1984163208b4e
YenKwanNee <cindyyen12@gmail.com>
2021-09-04 11:10:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapidemo/Model/MessageRespond.kt
@@ -0,0 +1,7 @@
+package com.example.myapidemo.Model
+
+data class MessageRespond(
+
+    val message: String
+
+)
---
+++ app/src/main/java/com/example/myapidemo/api/MyRestAPI.kt
@@ -0,0 +1,30 @@
+package com.example.myapidemo.api
+
+import com.example.myapidemo.Model.MessageRespond
+import com.example.myapidemo.Model.StudentRespond
+import retrofit2.Call
+import retrofit2.http.*
+
+interface MyRestAPI {
+
+    companion object{
+
+        val BASE_URL :String = "http://demo.onmyfinger.com/"
+
+    }
+
+    @GET("home/getall")
+    fun getAll(): Call<List<StudentRespond>>
+
+
+    @GET("home/getbyid")
+    fun getById(@Query("id") id:String): Call<StudentRespond>
+
+    @POST("home/add")
+    @FormUrlEncoded
+    fun add(
+        @Field("id") id: String,
+        @Field("name") name: String,
+        @Field("programme") programme: String,
+        @Field("img") img: String) : Call <MessageRespond>
+}
